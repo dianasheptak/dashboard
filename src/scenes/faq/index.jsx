eef95e1b14961f4329eabaf8e0847a4746803995
dianasheptak <a234@234s-MacBook-Pro.local>
2022-11-16 21:02:55 +0200

    first commit

---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,5 @@
+export default function hello() {
+    return (
+         <h1 style={{textAlign: 'center', marginTop:'60px', fontSize: 40, fontWeight: 300}}>Sorry, the FAQ pages haven't been filled out yet...</h1>
+    )
+}
